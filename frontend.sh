daff27c92f05ccd4d995cd9b0567e4820d075352
naren <naren.gandham08@gmail.com>
2023-05-03 00:26:52 +0530

    made config changes in frontend.

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,15 @@
 ## -->>>Install Nginx
 echo Installing nginx
 yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-echo started nginx
-sleep 10
-tail -f /var/log/nginx/error.log
-cd /home/centos/devops
+cp frontend.config /etc/nginx/default.d/roboshop.conf
 ##--> Remove the default content that web server is serving.
 rm -rf /usr/share/nginx/html/*
-
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 ##--->copy config/Create Nginx Reverse Proxy Configuration.
 echo creating nginx proxy configuration file
-#cp frontendconfig /etc/nginx/default.d/roboshop.conf
+
 ##--->Start & Enable Nginx service
 systemctl enable nginx
 systemctl restart nginx
