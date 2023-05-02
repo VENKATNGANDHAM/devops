24ddb54bba26f5ef1db38470ad5403dc60b679e0
naren <naren.gandham08@gmail.com>
2023-05-02 17:42:32 +0530

    frontend changes

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,16 @@
-sudo -i
-##--> Remove the default content that web server is serving.
-rm -rf /usr/share/nginx/html/*
-##--->copy config/Create Nginx Reverse Proxy Configuration.
-echo creating nginx proxy configuration file
-cp frontendconfig /etc/nginx/default.d/roboshop.conf
 ## -->>>Install Nginx
 echo Installing nginx
 yum install nginx -y
+
+##--> Remove the default content that web server is serving.
+rm -rf /usr/share/nginx/html/*
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+##--->copy config/Create Nginx Reverse Proxy Configuration.
+echo creating nginx proxy configuration file
+cp frontendconfig /etc/nginx/default.d/roboshop.conf
 ##--->Start & Enable Nginx service
 systemctl enable nginx
 systemctl restart nginx
