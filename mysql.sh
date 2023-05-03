e84f8ff064b198e02ad7fc33e54d99da7662ab00
naren <naren.gandham08@gmail.com>
2023-05-03 20:48:33 +0530

    mysql changes

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,11 @@
-sudo -i
 dnf module disable mysql -y
-##Setup the MySQL5.7 repo file
-cp mysqlrepo /etc/yum.repos.d/mysql.repo
-#Install MySQL Server
+echo ##Setup the MySQL5.7 repo file
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo #Install MySQL Server
 yum install mysql-community-server -y
-##Start MySQL Service
+echo  ##Start MySQL Service
 systemctl enable mysqld
 systemctl restart mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
-##check this after loggininto sql
+echo ##check this after loggininto sql
 mysql -uroot -pRoboShop@1
