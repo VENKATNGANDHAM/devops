dd23425662b4a2a6611e2532a667e0580953b1c0
naren <naren.gandham08@gmail.com>
2023-05-03 20:33:14 +0530

    cart changes

--- cart.sh
+++ cart.sh
@@ -1,21 +1,21 @@
-sudo -i
-##Setup NodeJS repos. Vendor is providing a script to setup the repos.
+echo Setup NodeJS repos. Vendor is providing a script to setup the repos.
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-##Install Nodejs
+echo Setup SystemD Cart Service
+cp cart.service /etc/systemd/system/cart.service
+echo Install Nodejs
 yum install nodejs -y
-##Add application user
+echo Add application user
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
-##Lets download the dependencies.
+echo Lets download the dependencies.
 cd /app
 npm install
-##Setup SystemD Cart Service
-cp cartservice /etc/systemd/system/cart.service
-##Load the service.
+
+echo Load the service.
 systemctl daemon-reload
-##Start the service.
+echo  start the service.
 systemctl enable cart
 systemctl restart cart
