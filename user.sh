f3d95647241785275e3ed8716492d9af5d9f470e
naren <naren.gandham08@gmail.com>
2023-05-03 19:49:46 +0530

    user changes

--- user.sh
+++ user.sh
@@ -1,22 +1,22 @@
-sudo -i
-##Setup NodeJS repos. Vendor is providing a script to setup the repos.
+echo Setup NodeJS repos. Vendor is providing a script to setup the repos.
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-##Install NodeJS
+echoInstall NodeJS
 yum install nodejs -y
-##Add application User
+echo Setup SystemD User Service
+cp userservice.repo /etc/systemd/system/user.service
+echo Add application User
 useradd roboshop
-##Lets setup an app directory.
+echo Lets setup an app directory.
 mkdir /app
-##Download the application code to created app directory.
+echo Download the application code to created app directory.
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
 unzip /tmp/user.zip
-##Lets download the dependencies.
+echo Lets download the dependencies.
 cd /app
 npm install
-##Setup SystemD User Service
-cp userservice /etc/systemd/system/user.service
-##Load the service.
+
+echo Load the service.
 systemctl daemon-reload
 ##Start the service.
 systemctl enable user
@@ -25,8 +25,8 @@ systemctl restart user
 ##We need to load the schema. To load schema we need to install mongodb client.
   #
   #To have it installed we can setup MongoDB repo and install mongodb-client
-  cp mongodbrepo /etc/yum.repos.d/mongo.repo
+  cp mongodb.repo /etc/yum.repos.d/mongo.repo
   yum install mongodb-org-shell -y
-  ##Load Schema
+ echo Load Schema
 
-  mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
+  mongo --mongodb-dev.ngandham.online </app/schema/user.js
