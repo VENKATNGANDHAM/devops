855272b79c456ccb0225b3055bde2199eda208f2
naren <naren.gandham08@gmail.com>
2023-05-03 21:04:14 +0530

    payment changes

--- payment.sh
+++ payment.sh
@@ -1,18 +1,18 @@
-sudo -i
-##Install Python 3.6
+echo ##Install Python 3.6
 yum install python36 gcc python3-devel -y
+echo ##Setup SystemD Payment Service
+cp  payment.conf /etc/systemd/system/payment.service
 useradd roboshop
 mkdir /app
-##Download the application code to created app directory.
+echo ##Download the application code to created app directory.
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
 unzip /tmp/payment.zip
-##Lets download the dependencies.
+echo ##Lets download the dependencies.
 cd /app
 pip3.6 install -r requirements.txt
-##Setup SystemD Payment Service
-cp  paymentservice /etc/systemd/system/payment.service
+
 systemctl daemon-reload
-##Start the service.
+echo ##Start the service.
 systemctl enable payment
 systemctl restart payment
